002ac0e5456988adfa73e8999d66ee3cc3fad840
Yucheng0726 <139911094+Yucheng0726@users.noreply.github.com>
2024-08-02 23:34:46 +0800

    Update model_download.py

--- model_download.py
+++ model_download.py
@@ -1,8 +1,8 @@
-from diffusers import ControlNetPipeline
+from diffusers import StableDiffusionControlNetPipeline
 
 # 加载预训练模型
 model_id = "Hhhhhao97/ldm_imagenet_random_noise_2.5"
-pipeline = ControlNetPipeline.from_pretrained(model_id)
+pipeline = StableDiffusionControlNetPipeline.from_pretrained(model_id)
 
 # 保存模型到指定路径
 save_path = "runwayml/pretrainmodel"
